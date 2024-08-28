1423de2dce3d2386e62a8b89c24e702dc81681ae
Ari Lotter <arilotter@gmail.com>
2024-08-27 20:59:03 -0400

    ooo

--- coordinator/src/main.rs
+++ coordinator/src/main.rs
@@ -194,7 +194,10 @@ async fn main() -> Result<()> {
                         } = req
                         {
                             let message_size = data.len();
-                            info!("got message from {peer}, {}", convert_bytes(message_size));
+                            info!(
+                                "got message from {peer}, {}",
+                                convert_bytes(message_size as f64)
+                            );
                             data_received_per_tick
                                 .entry(peer)
                                 .or_insert_with(|| VecDeque::new())
